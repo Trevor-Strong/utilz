9fc8f3f4b2cd634998d140b3f16c038f923738a4
Trevor-Strong <76170333+Trevor-Strong@users.noreply.github.com>
2024-06-21 18:55:08 -0500

    Initial commit

---
+++ build.zig
@@ -0,0 +1,44 @@
+const std = @import("std");
+
+const Build = std.Build;
+
+pub fn build(b: *Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const pic = b.option(bool, "pic", "Generate Position Independent Code");
+    const check_fmt = b.option(
+        bool,
+        "check_fmt",
+        "Check the formatting of files instead of fixing it",
+    ) orelse false;
+
+    const root_source_file = b.path("src/util.zig");
+
+    const module = b.addModule("util", .{
+        .target = target,
+        .optimize = optimize,
+        .root_source_file = root_source_file,
+        .pic = pic,
+    });
+    _ = module;
+
+    // Formatting
+
+    const fmt = b.addFmt(.{ .paths = &.{ "src", "build.zig", "build.zig.zon" }, .check = check_fmt });
+
+    const fmt_step = b.step("fmt", "'zig fmt' the source files");
+    fmt_step.dependOn(&fmt.step);
+
+    // Test
+
+    const unit_tests = b.addTest(.{
+        .root_source_file = root_source_file,
+        .target = target,
+        .optimize = optimize,
+        .pic = pic,
+    });
+
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_unit_tests.step);
+}
